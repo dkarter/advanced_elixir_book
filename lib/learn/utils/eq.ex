b73bd305928001cce7f14a48bddd4e1305ba7bf0
Dorian Karter <dkarter@users.noreply.github.com>
2026-03-08 00:00:55 -0600

    fix: use generic eq impl by id

--- lib/learn/utils/eq.ex
+++ lib/learn/utils/eq.ex
@@ -7,6 +7,9 @@ defprotocol Learn.Utils.Eq do
 end
 
 defimpl Learn.Utils.Eq, for: Any do
+  def eq?(%{id: id1}, %{id: id2}), do: id1 == id2
   def eq?(a, b), do: a == b
+
+  def not_eq?(%{id: id1}, %{id: id2}), do: id1 != id2
   def not_eq?(a, b), do: a != b
 end
